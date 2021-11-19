a48af63953ba0021c6c70d52a6f446bd3a963831
dinh giang <truonggiangdinh1862@gmail.com>
2021-11-20 00:18:19 +0700

    update

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -13,21 +13,44 @@ import React, { Component, useEffect, useState } from 'react';
 import { db } from "../../firebase";
 export default function Product() {
 	const { productId } = useParams()
-	const [data,setData] = useState({});
+	const [data, setData] = useState({});
+	const [price,setPrice] = useState(0)
+	const [status,setStatus] = useState()
 	useEffect(() => {
 		fetchProduct()
-		console.log('dddd',productId)
-
 	}, [productId])
 	const fetchProduct = async () => {
-		console.log('dddd',productId)
+		console.log('dddd', productId)
 		await db.collection("products").doc(productId).get().
-			then(doc =>setData(doc.data()))
+			then((doc) => {
+				const list = {
+					id:doc.id,
+					image: Object.values(doc.data().image),
+					name:doc.data().name,
+					price:doc.data().price,
+					status:doc.data().status,
+					style:doc.data().style
+					// price:doc.data().price
+				}
+				console.log(list);
+				setPrice(doc.data().price)
+				setStatus(doc.data().status)
+				setData(list)
+			})
+	}
+	const onChange=(event)=>{
+		console.log(event.target.value)
+		setPrice(event.target.value)
+		
+	}
+	const handleChange=(event)=>{
+		// console.log(event.target)
+		setStatus(event.target.value)
 	}
 	return (
 		<div className="product">
 			<div className="productTitleContainer">
-				<h1 className="productTitle">Product</h1>
+				<h1 className="productTitle">{data ? data.name : ''}</h1>
 				<Link to="/newproduct">
 					<button className="productAddButton">Create</button>
 				</Link>
@@ -38,56 +61,57 @@ export default function Product() {
 				</div> */}
 				<div className="productTopRight">
 					<div className="productInfoTop">
-						<img
-							src="https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+						{data.image?<img
+							src={data.image[0]}
 							alt=""
 							className="productInfoImg"
-						/>
-						<span className="productName">{data?data.fashion:''}</span>
+						/>:null}
+						<span className="productName">{data ? data.fashion : ''}</span>
 					</div>
 					<div className="productInfoBottom">
 						<div className="productInfoItem">
-							<span className="productInfoKey">id:</span>
-							<span className="productInfoValue">123</span>
+							<span className="productInfoKey">Trạng thái</span>
+							<span className="productInfoValue">{data ? data.status : ''}</span>
 						</div>
 						<div className="productInfoItem">
-							<span className="productInfoKey">sales:</span>
-							<span className="productInfoValue">5123</span>
+							<span className="productInfoKey">Giá</span>
+							<span className="productInfoValue">{data ? data.price : ''}đ</span>
 						</div>
-						<div className="productInfoItem">
+						{/* <div className="productInfoItem">
 							<span className="productInfoKey">active:</span>
 							<span className="productInfoValue">yes</span>
 						</div>
 						<div className="productInfoItem">
 							<span className="productInfoKey">in stock:</span>
 							<span className="productInfoValue">no</span>
-						</div>
+						</div> */}
 					</div>
 				</div>
 			</div>
 			<div className="productBottom">
 				<form className="productForm">
 					<div className="productFormLeft">
-						<label>Product Name</label>
-						<input type="text" placeholder="Apple AirPod" />
-						<label>In Stock</label>
+						<label>Giá</label>
+						<input value={price} type="number" onChange={onChange} placeholder="Thay đổi giá" />
+						{/* <label>Thay đổi trạng thái</label>
 						<select name="inStock" id="idStock">
 							<option value="yes">Yes</option>
 							<option value="no">No</option>
-						</select>
-						<label>Active</label>
-						<select name="active" id="active">
-							<option value="yes">Yes</option>
-							<option value="no">No</option>
+						</select> */}
+						<label>Thay đổi trạng thái</label>
+						<select value={status} onChange={handleChange} name="active" id="active">
+							<option value="New Arrive">New Arrive</option>
+							<option value="Sale Off">Sale Off</option>
+							<option value="Best Seller">Best Seller</option>
 						</select>
 					</div>
 					<div className="productFormRight">
 						<div className="productUpload">
-							<img
-								src="https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+							{data.image?<img
+								src={data.image[0]}
 								alt=""
 								className="productUploadImg"
-							/>
+							/>:null}
 							<label for="file">
 								<Publish />
 							</label>
